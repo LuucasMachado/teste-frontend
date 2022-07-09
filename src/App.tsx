09e62b72d93be9a71ae5b05c560946b4ec1c4152
Lucas Dourado <lucashenrique.2d@gmail.com>
2022-07-09 19:23:38 -0300

    busca e melhorias

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import UserList from './components/UserList'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faList } from '@fortawesome/free-solid-svg-icons';
-import { faGrip } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { MDBDataTableV5 } from 'mdbreact';
 
 
 function App() {
@@ -23,7 +23,36 @@ function App() {
   }
 
 
-  const [users, setUserList] = useState<IPosts[]>([]);
+  var [posts, setPostList] = useState<IPosts[]>([]);
+
+  const [datatable, setDatatable] = useState({
+    columns: [
+    {
+      label: 'userId',
+      field: 'userId',
+      attributes: {
+        'aria-controls': 'DataTable',
+        'aria-label': 'userId',
+      },
+    },
+    {
+      label: 'Title',
+      field: 'title',
+      width: 300,
+      attributes: {
+        'aria-controls': 'DataTable',
+        'aria-label': 'Title',
+      },
+    },
+    {
+      label: 'Body',
+      field: 'body',
+      attributes: {
+        'aria-controls': 'DataTable',
+        'aria-label': 'Body',
+      },
+    },
+  ],rows: posts})
   
   interface ISelectedViewOption {
     value: string;
@@ -37,18 +66,39 @@ function App() {
 
 
   const getApiData = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts"
+    var filter_parms = ''
+    if(searchValue) {
+      filter_parms = `?title=${searchValue}`;
+    }
+    const response = fetch(
+      `https://jsonplaceholder.typicode.com/posts${filter_parms}`
     ).then((response) => response.json())
-     .then(data =>  setUserList(data));
-  
+      .then(data =>  {
+      setPostList(data);
+    });
+
     // update the state
-    console.log(users);
+    console.log(posts);
   };
 
   useEffect(() => {
     getApiData();
   }, []);
+
+  var [searchValue, setSearchValue] = useState("")
+
+  const handleSearch = () => {
+    getApiData();
+  }
+
+  const handleClear = () => {
+    setSearchValue('');
+    getApiData();
+  }
+
+  const changeValue = (value) => {
+    console.log(value)
+  }
 
   return (
     <div className="App">
@@ -71,10 +121,10 @@ function App() {
             <Form className="d-flex d-flex align-items-baseline mb-4">
 
               <Form.Group className="w-50 m-3" controlId="formBasicTitle">
-                <Form.Control type="title" placeholder="Type for search..." />
+                <Form.Control type="title" placeholder="Type for search..." value={searchValue} onChange={e => setSearchValue(e.target.value)} />
               </Form.Group>
 
-              <Form.Select aria-label="Select type list" className="w-25 m-3">
+              <Form.Select aria-label="Select type list" className="w-25 m-3" onChange={e => changeValue(e.target.value)}>
                 {selectedOptionView.map(option => {
                   return (
                   <option value={option.value} key={option.value}>
@@ -84,15 +134,51 @@ function App() {
                 })}
               </Form.Select>
 
-              <Button variant="primary" type="submit" className="m-1">
+              <Button variant="primary"  className="m-1" onClick={handleSearch}>
                 Search
               </Button>
 
-              <Button variant="light" className="m-1 text-uppercase">
+              <Button variant="light" className="m-1 text-uppercase" onClick={handleClear}>
                 Clear
               </Button>
             </Form>
-            <UserList items={users}  />
+            {/* <UserList items={posts}  /> */}
+            <MDBDataTableV5 
+              hover 
+              entriesOptions={[5, 20, 25]}
+              entries={5}
+              // searchTop
+              searching={false}
+              pagesAmount={4} data={{
+                columns: [
+                {
+                  label: 'POSTID',
+                  field: 'postid',
+                  width: 150,
+                  attributes: {
+                    'aria-controls': 'DataTable',
+                    'aria-label': 'PostId',
+                  },
+                },
+                {
+                  label: 'Title',
+                  field: 'title',
+                  width: 150,
+                  attributes: {
+                    'aria-controls': 'DataTable',
+                    'aria-label': 'Title',
+                  },
+                },
+                {
+                  label: 'Body',
+                  field: 'body',
+                  width: 150,
+                  attributes: {
+                    'aria-controls': 'DataTable',
+                    'aria-label': 'Body',
+                  },
+                },
+              ],rows: posts}} />
           </Col>
         </Row>
     </Container>
