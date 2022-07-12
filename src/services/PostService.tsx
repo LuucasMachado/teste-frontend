642baf1e09819f3e64477359a35cdf0b26017cea
Lucas Dourado <lucashenrique.2d@gmail.com>
2022-07-12 14:43:09 -0300

    aplicando fix na pesquisa

--- src/services/PostService.tsx
+++ src/services/PostService.tsx
@@ -3,10 +3,12 @@ export default {
     async getApiData (searchValue:string, page:number) {
         var filter_parms = ''
         if(searchValue) {
-          filter_parms = `?title=${searchValue}`;
+          filter_parms = `?title=${searchValue}&_page=${page}&_limit=10`;
+        } else {
+          filter_parms = `?_page=${page}&_limit=10`;
         }
         return fetch(
-          `https://jsonplaceholder.typicode.com/posts${filter_parms}?_page=${page}&_limit=10`
+          `https://jsonplaceholder.typicode.com/posts${filter_parms}`
         )
       }
   };
